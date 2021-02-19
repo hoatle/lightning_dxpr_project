90d4621031315f073a2e5c5098ac9233f9de6454
hoatle <hoatle@teracy.com>
2021-02-19 19:17:01 +0700

    test

--- run-dev.sh
+++ run-dev.sh
@@ -5,10 +5,15 @@ rm -rf vendor docroot composer.lock
 
 composer config --global bearer.packages.dxpr.com $DXPR_ACCESS_TOKEN
 
-# debug: check if cached by github action
+# debug for composer 2 before: check if cached by github action
 curl https://packages.dxpr.com/8/p2/dxpr/dxpr_builder.json
 
 composer install -vvv
 
-# debug
-# cat /tmp/cache/repo/https---packages.dxpr.com-8/provider-dxpr~dxpr-builder.json
+# debug for composer 2 after:
+
+FILE=/tmp/cache/repo/https---packages.dxpr.com-8/provider-dxpr~dxpr-builder.json
+if test -f "$FILE"; then
+  echo "cat $FILE"
+  cat $FILE
+fi
