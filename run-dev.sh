2d8c29d16ca46f34e56ebebe281500842fb25669
hoatle <hoatle@teracy.com>
2021-02-19 18:01:32 +0700

    test

--- run-dev.sh
+++ run-dev.sh
@@ -5,4 +5,10 @@ rm -rf vendor docroot composer.lock
 
 composer config --global bearer.packages.dxpr.com $DXPR_ACCESS_TOKEN
 
+# debug: check if cached by github action
+curl https://packages.dxpr.com/8/p2/dxpr/dxpr_builder.json
+
 composer install -vvv
+
+# debug
+# cat /tmp/cache/repo/https---packages.dxpr.com-8/provider-dxpr~dxpr-builder.json
